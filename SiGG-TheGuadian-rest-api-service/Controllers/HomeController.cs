c4ffdfea9a38d79135db25c22c7292eb392d225c
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-22 11:51:52 +0700

    SiGG-TheGuadian-rest-api-service-Init-component

---
+++ SiGG-TheGuadian-rest-api-service/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SiGG_TheGuadian_rest_api_service.Models;
+using System.Diagnostics;
+
+namespace SiGG_TheGuadian_rest_api_service.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SiGG-TheGuadian-rest-api-service/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SiGG_TheGuadian_rest_api_service.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
